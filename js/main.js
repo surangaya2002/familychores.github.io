cdec7b6f18ffcb124743880bc60edbe82af89b24
eriksoren <eriksrln@gmail.com>
2022-04-07 17:13:27 +0200

    added function to add family goal in parent view and fixed layout issues in childrenview

--- js/main.js
+++ js/main.js
@@ -17,3 +17,13 @@ function navigateLogin() {
       window.location.href = "childview.html";
     }
   }
+
+function addFamilyGoal() {
+    var title = document.getElementById("goal_title").value;
+    //var description = document.getElementById("goal_description").value; 
+    //var reward = document.getElementById("goal_reward").value;  
+
+    // Create new li element 
+    var newList = "<li class='list-group-item'>" + title + "</li>"; 
+    $(".family_score_list").append(newList); 
+}
