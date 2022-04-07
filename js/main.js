a63026b5b66c17a10c366e80aa8069392dee4c90
eriksoren <eriksrln@gmail.com>
2022-04-07 12:30:42 +0200

    added default page and login page

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+}); 
+
+
+function navigateLogin() {
+     
+    var username = document.getElementById("floatingInput").value;
+    var password = document.getElementById("floatingPassword").value;
+
+    if(username == 'parent' && password == 'parent')
+    {
+      window.location.href = "parentview.html";
+    }
+    else if(username == 'child' && password == 'child')
+    {
+      window.location.href = "childview.html";
+    }
+  }
